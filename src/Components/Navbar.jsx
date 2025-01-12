5bf60e3157c87caa497ea6fdb0c0ab4287eb4c83
rajarshidattapy <rayan05rio@gmail.com>
2025-01-12 16:16:27 +0530

    fixed navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,105 +4,113 @@ import Navbar from 'react-bootstrap/Navbar';
 import { useFirebase } from '../Context/firebase';
 
 function ColorSchemesExample() {
-  const{user,signOut}=useFirebase();
+  const { user, signOut } = useFirebase();
   return (
     <>
-     <Navbar
-  style={{
-    // backgroundColor: "#B2EC5D",
-    backgroundColor:"black",
-    border: "3px solid black", // Add black solid border
-    borderRadius: "3px", // Set border radius to 3px
-  }}
-  data-bs-theme="dark"
-  className="navbar"
->
-  <Container>
-    <Navbar.Brand
-      href="/"
-      style={{
-        color: "white",
-        fontWeight: "bold",
-      }}
-    >
-      Med-Saathi
-    </Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link
-        href="/"
+      <Navbar
         style={{
-          color: "white",
+          backgroundColor: "black",
+          border: "3px solid black",
+          borderRadius: "3px",
         }}
+        data-bs-theme="dark"
+        className="navbar"
       >
-        Home
-      </Nav.Link>
-      <Nav.Link
-        href="patientForm"
-        style={{
-          color: "white",
-        }}
-      >
-        Patient Form
-      </Nav.Link>
-      <Nav.Link
-        href="dispensary"
-        style={{
-          color: "white",
-        }}
-      >
-        Dispensary
-      </Nav.Link>
-      {!user ? (
-        <>
-          <Nav.Link
-            href="login"
+        <Container>
+          <Navbar.Brand
+            href="/"
             style={{
               color: "white",
-           
+              fontWeight: "bold",
+              display: "flex",
+              alignItems: "center", // Align logo and text vertically
             }}
           >
-            Login
-          </Nav.Link>
-          <Nav.Link
-            href="register"
-            style={{
-              color: "white",
-              
-            }}
-          >
-            Register
-          </Nav.Link>
-          <Nav.Link
-            href="PatientUI"
-            style={{
-              color: "white",
-            }}
-          >
-            Patient UI
-          </Nav.Link>
-        </>
-      ) : (
-        <button
-          style={{
-            color: "white",
-            fontWeight: "bold",
-            backgroundColor: "transparent",
-            border: "none",
-            cursor: "pointer",
-          }}
-          onClick={() => {
-            signOut();
-          }}
-        >
-          Logout
-        </button>
-      )}
-    </Nav>
-  </Container>
-</Navbar>
-
+            <img
+              src="https://i.imgur.com/RemPX1F.png"
+              alt="Med-Saathi Logo"
+              style={{
+                width: "40px", // Adjust logo size
+                height: "40px",
+                marginRight: "10px", // Add spacing between logo and text
+              }}
+            />
+            Med-Saathi
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link
+              href="/"
+              style={{
+                color: "white",
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="patientForm"
+              style={{
+                color: "white",
+              }}
+            >
+              Patient Form
+            </Nav.Link>
+            <Nav.Link
+              href="dispensary"
+              style={{
+                color: "white",
+              }}
+            >
+              Dispensary
+            </Nav.Link>
+          </Nav>
+          <Nav className="ml-auto">
+            {!user ? (
+              <>
+                <Nav.Link
+                  href="PatientUI"
+                  style={{
+                    color: "white",
+                  }}
+                >
+                  Patient UI
+                </Nav.Link>
+                <Nav.Link
+                  href="register"
+                  style={{
+                    color: "white",
+                  }}
+                >
+                  Register
+                </Nav.Link>
+                <Nav.Link
+                  href="login"
+                  style={{
+                    color: "white",
+                  }}
+                >
+                  Login
+                </Nav.Link>
+              </>
+            ) : (
+              <button
+                style={{
+                  color: "white",
+                  fontWeight: "bold",
+                  backgroundColor: "transparent",
+                  border: "none",
+                  cursor: "pointer",
+                }}
+                onClick={() => {
+                  signOut();
+                }}
+              >
+                Logout
+              </button>
+            )}
+          </Nav>
+        </Container>
+      </Navbar>
       <br />
-    
     </>
   );
 }
